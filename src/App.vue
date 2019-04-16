4fece0871f469c02850e95b6c40a596163e516a8
Allan Dantas <allancruz716@gmail.com>
2019-04-16 17:10:42 -0300

    Atualizar vue CLi para versão 3

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
-    <div>
-        <Header></Header>
-        <Home></Home>
-        <Footer></Footer>
-    </div>
+  <div>
+    <Header></Header>
+    <Home></Home>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
-    import Header from './components/header/Header.vue';
-    import Home from './components/home/Home.vue';
-    import Footer from './components/footer/Footer.vue';
-    export default {
-        components: {
-            'Header' : Header,
-            'Home' : Home,
-            'Footer' : Footer
-        },
-    }
+  import Header from './components/header/Header.vue';
+  import Home from './components/home/Home.vue';
+  import Footer from './components/footer/Footer.vue';
+  export default {
+    components: {
+      'Header' : Header,
+      'Home' : Home,
+      'Footer' : Footer
+    },
+  }
 </script>
 
 <style scoped lang="scss">
-    @import "App";
+  @import "App";
 </style>
